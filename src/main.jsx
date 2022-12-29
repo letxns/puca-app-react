4db728323b4a93ac84ab24027064590ddbec4b18
letxns <lele_martins1999@yahoo.com.br>
2022-12-29 10:17:14 -0300

    feat: cria container principal e add pacotes necessarios

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider, Route, } from 'react-router-dom';
+import Home from "./routes/Home"
+import NewForm from "./routes/NewForm"
+import ConfigForm from "./routes/ConfigForm"
 import App from './App'
+import './Index.css'
+
+const router = createBrowserRouter([
+    {
+        element: <App />,
+        children: [
+            {
+                path: "/",
+                element: <Home />
+            },
+            {
+                path: "/form",
+                element: <NewForm />
+            },
+            {
+                path: "/config-form",
+                element: <ConfigForm />
+            }
+        ]
+    }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>,
 )
